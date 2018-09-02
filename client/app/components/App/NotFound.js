01862142e0cb480f25b05d7fca993c6e3396444c
HamdiSheikha <42714268+HamdiSheikha@users.noreply.github.com>
2018-09-02 15:39:16 +0300

    Update NotFound.js

--- client/app/components/App/NotFound.js
+++ client/app/components/App/NotFound.js
@@ -2,11 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <>
-    <h2>Page not found</h2>
+    <div class = "bodyDiv">
+    <div class="backgroundDiv">
 
-    <Link to="/">Go home</Link>
-  </>
+    <div class="container">
+
+    	<div  class="error">
+    		<p class="p">4</p>
+    		<span class="dracula">
+    			<div class="con">
+    				<div class="hair"></div>
+    				<div class="hair-r"></div>
+    				<div class="head"></div>
+        		<div class="eye"></div>
+        		<div class="eye eye-r"></div>
+      			<div class="mouth"></div>
+      			<div class="blod"></div>
+      			<div class="blod blod2"></div>
+    			</div>
+    		</span>
+    		<p class="p">4</p>
+
+    		<div class="page-ms">
+    			<p class="page-msg"> Oops, the page you're looking for Disappeared </p>
+    			<a href="http://localhost:8080/"><button class="go-back">Go Back</button></a>
+    		</div>
+    </div>
+    	</div>
+      </div>
+      </div>
+
 );
 
 export default NotFound;
